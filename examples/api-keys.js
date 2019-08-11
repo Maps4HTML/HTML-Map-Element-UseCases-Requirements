039d09700abb68160f5432dc7ed57972117f5171
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-11 03:45:59 +0000

    Deploy Maps4HTML/HTML-Map-Element-UseCases-Requirements to github.com/Maps4HTML/HTML-Map-Element-UseCases-Requirements.git:gh-pages

--- examples/api-keys.js
+++ examples/api-keys.js
@@ -6,4 +6,5 @@ var m4h = m4h || {};
 m4h.keys = {
   google: 'AIzaSyCP1y_kwKFUy_jBDb-4EvUjt7b5jCYftfc',
   mapboxGL: 'pk.eyJ1IjoibWFwczRodG1sLWFtZWxpYWJyIiwiYSI6ImNqeXhrampmMTB3d2EzZ3BlenFvMHVicmIifQ.zE60jyQ0QH_NlKFyo9EmvA',
+  tomtom: 'kcm72J917QftQG43eSwuUeKmm5fyni3G',
 };

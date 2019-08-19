13e5586b72bcfcbd10f9b6f8d129d34590f0acb4
Nick Fitzsimons <nick@nickfitz.co.uk>
2019-08-19 18:05:10 +0100

    Add a temporary key for Bing Maps API.

--- examples/api-keys.js
+++ examples/api-keys.js
@@ -7,4 +7,5 @@ m4h.keys = {
   google: 'AIzaSyCP1y_kwKFUy_jBDb-4EvUjt7b5jCYftfc',
   mapboxGL: 'pk.eyJ1IjoibWFwczRodG1sLWFtZWxpYWJyIiwiYSI6ImNqeXhrampmMTB3d2EzZ3BlenFvMHVicmIifQ.zE60jyQ0QH_NlKFyo9EmvA',
   tomtom: 'kcm72J917QftQG43eSwuUeKmm5fyni3G',
+  bing: 'AmD9pKWiGBXZ6XrD5jWzzJ5ikdkMiXjhLMbb8_drsxVFiyS_jjqDAbRYtkcaKI7r',
 };

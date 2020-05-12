f5d04e8fecf48c6631a90431fed35d56b9604abc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-12 01:48:46 +0000

    Deploy Maps4HTML/HTML-Map-Element-UseCases-Requirements to github.com/Maps4HTML/HTML-Map-Element-UseCases-Requirements.git:gh-pages

--- examples/data/fisk-maps.js
+++ examples/data/fisk-maps.js
@@ -12,7 +12,7 @@ m4h.fiskMaps = {
           "attribution": "Presidential Mississippi River Commission, 1944. Public Domain.",
           "scheme": "tms",
           "tiles": [
-            "https://maps4html.github.io/FiskMississippiMapsStreamCourses/sheet01/{z}/{x}/{-y}.png"
+            "https://maps4html.org/FiskMississippiMapsStreamCourses/sheet01/{z}/{x}/{-y}.png"
           ],
           "bounds": [-92.73821874999945, 35.15156827115307, -88.58807837153826, 37.38372501845145],
           "minzoom": 6,
@@ -28,7 +28,7 @@ m4h.fiskMaps = {
           "attribution": "Presidential Mississippi River Commission, 1944. Public Domain.",
           "scheme": "tms",
           "tiles": [
-            "https://maps4html.github.io/FiskMississippiMapsStreamCourses/sheet02/{z}/{x}/{-y}.png"
+            "https://maps4html.org/FiskMississippiMapsStreamCourses/sheet02/{z}/{x}/{-y}.png"
           ],
           "bounds": [-92.70756545472462, 33.02407078465588, -88.65406790680284, 35.27414929261117],
           "minzoom": 6,
@@ -44,7 +44,7 @@ m4h.fiskMaps = {
           "attribution": "Presidential Mississippi River Commission, 1944. Public Domain.",
           "scheme": "tms",
           "tiles": [
-            "https://maps4html.github.io/FiskMississippiMapsStreamCourses/sheet03/{z}/{x}/{-y}.png"
+            "https://maps4html.org/FiskMississippiMapsStreamCourses/sheet03/{z}/{x}/{-y}.png"
           ],
           "bounds": [-92.64710999091871, 30.90807578948790, -88.67752165286296, 33.17549574297950],
           "minzoom": 6,
@@ -60,7 +60,7 @@ m4h.fiskMaps = {
           "attribution": "Presidential Mississippi River Commission, 1944. Public Domain.",
           "scheme": "tms",
           "tiles": [
-            "https://maps4html.github.io/FiskMississippiMapsStreamCourses/sheet04/{z}/{x}/{-y}.png"
+            "https://maps4html.org/FiskMississippiMapsStreamCourses/sheet04/{z}/{x}/{-y}.png"
           ],
           "bounds": [-92.60431421311640, 28.79036396296392, -88.72531489137181, 31.04551669046586],
           "minzoom": 6,
